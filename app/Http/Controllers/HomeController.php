7c236c4f3093639576b492e5a9c375bcfb810177
François Dulmet <fdulmet@gmail.com>
2017-03-21 19:55:47 +0100

    classement en cours

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -27,9 +27,9 @@ class HomeController extends Controller
         $game = Game::find(1);
         $lieu = $game->lieu;
 
+        //chaque ligne
         $games = Game::all();
         $stats = [];// Array of games statistics
-
         $i = 0;
         foreach ($games as $game){
             $stats[$i] = [];
@@ -56,16 +56,151 @@ class HomeController extends Controller
 
                 $y++;
             }
-
             $i++;
         }
 
-        //CLASSEMENT
 
-        //VIEW
+        //CLASSEMENT
+        $equipes = Equipe::all();
+        $rang = 0;
+        $statsClassement = [];// Array of team statistics
+        $j = 0;
+        foreach ($equipes as $equipe){
+            $statsClassement[$j] = [];
+
+            //Rang
+            $rang = $rang+1;
+            $statsClassement[$j]['rang'] = $rang;
+
+            //Equipe
+            $nomEquipe = $equipe->nom;
+            $statsClassement[$j]['nomEquipe'] = $nomEquipe;
+
+            //buts pour
+            $z = 1;
+            $butsPour = 0;
+            foreach ($equipe->games as $game){
+                $butsPourDeChaqueMatch = $game->pivot->buts;
+                $butsPour += $butsPourDeChaqueMatch;
+                $z++;
+            }
+            $statsClassement[$j]['butsPour'] = $butsPour;
+
+            //buts contre
+            foreach ($equipe->games as $game){
+                $game_id = $game->pivot->game_id;
+                $equipe_id = $game->pivot->equipe_id;
+                $autre_equipe_game = DB::table('equipe_game')->where([
+                    ['game_id','=', $game_id],
+                    ['equipe_id','!=', $equipe_id],
+                ])->get();
+
+                $zz = 1;
+                $butsContre = 0;
+                foreach ($autre_equipe_game as $autre_equipe_game){
+                    $butsContreDeChaqueMatch = $autre_equipe_game->buts;
+                    $butsContre += $butsContreDeChaqueMatch;
+                    $zz++;
+                }
+                $statsClassement[$j]['butsContre'] = $butsContre;
+            }
+
+            //diff.
+            $diff = $butsPour - $butsContre;
+            $statsClassement[$j]['diff'] = $diff;
+
+            //Joués
+            $joues = $equipe->games->count();
+            $statsClassement[$j]['joues'] = $joues;
+
+            //Gagnés
+            $a = 1;
+            foreach ($equipe->games as $game){
+                $butsPourDeChaqueMatch = $game->pivot->buts;
+                $game_id = $game->pivot->game_id;
+                $equipe_id = $game->pivot->equipe_id;
+                $autre_equipe_game = DB::table('equipe_game')->where([
+                    ['game_id','=', $game_id],
+                    ['equipe_id','!=', $equipe_id],
+                ])->get();
+                foreach ($autre_equipe_game as $autre_equipe_game){
+                    $butsContreDeChaqueMatch = $autre_equipe_game->buts;
+                }
+                $diffParMatch = $butsPourDeChaqueMatch - $butsContreDeChaqueMatch;
+                $gagnes = 0;
+                if ($diffParMatch>0){
+                    $gagneChaqueMatch = 1;
+                } else{
+                    $gagneChaqueMatch = 0;
+                }
+                $gagnes += $gagneChaqueMatch;
+                $a++;
+            }
+            $statsClassement[$j]['gagnes'] = $gagnes;
+
+            //Nuls
+            $b = 1;
+            foreach ($equipe->games as $game){
+                $butsPourDeChaqueMatch = $game->pivot->buts;
+                $game_id = $game->pivot->game_id;
+                $equipe_id = $game->pivot->equipe_id;
+                $autre_equipe_game = DB::table('equipe_game')->where([
+                    ['game_id','=', $game_id],
+                    ['equipe_id','!=', $equipe_id],
+                ])->get();
+                foreach ($autre_equipe_game as $autre_equipe_game){
+                    $butsContreDeChaqueMatch = $autre_equipe_game->buts;
+                }
+                $diffParMatch = $butsPourDeChaqueMatch - $butsContreDeChaqueMatch;
+                $nuls = 0;
+                if ($diffParMatch=0){
+                    $nulChaqueMatch = 1;
+                } else{
+                    $nulChaqueMatch = 0;
+                }
+                $nuls += $nulChaqueMatch;
+                $b++;
+            }
+            $statsClassement[$j]['nuls'] = $nuls;
+
+            //Perdus
+            $c = 1;
+            foreach ($equipe->games as $game){
+                $butsPourDeChaqueMatch = $game->pivot->buts;
+                $game_id = $game->pivot->game_id;
+                $equipe_id = $game->pivot->equipe_id;
+                $autre_equipe_game = DB::table('equipe_game')->where([
+                    ['game_id','=', $game_id],
+                    ['equipe_id','!=', $equipe_id],
+                ])->get();
+                foreach ($autre_equipe_game as $autre_equipe_game){
+                    $butsContreDeChaqueMatch = $autre_equipe_game->buts;
+                }
+                $diffParMatch = $butsPourDeChaqueMatch - $butsContreDeChaqueMatch;
+                $perdus = 0;
+                if ($diffParMatch<0){
+                    $perduChaqueMatch = 1;
+                } else{
+                    $perduChaqueMatch = 0;
+                }
+                $perdus += $perduChaqueMatch;
+                $c++;
+            }
+            $statsClassement[$j]['perdus'] = $perdus;
+
+            //Points
+            $points = 3*$gagnes + $nuls;
+            $statsClassement[$j]['points'] = $points;
+
+            $j++;
+        }
+
+
+    //VIEW
         return view('/home')->with([
             'lieu' => $lieu,
             'stats' => $stats,
+            'statsClassement' => $statsClassement,
             'confirmation' => $request->input('confirmation', null),
             //on utilise resquest pour aller chercher confirmation là où elle est définie,
             //en l'occurence dans les invitations controllers
--- resources/views/content/_classement.blade.php
+++ resources/views/content/_classement.blade.php
@@ -14,154 +14,18 @@
         <td>bc</td>
         <td>diff.</td>
     </tr>
-    <?php
-        $rang = 1;
-        $equipes = \App\Equipe::all();
-        foreach ($equipes as $equipe){
-    ?>
-    <tr>
-        <td>
-            <?php
-                //Rang
-                echo $rang;
-                $rang=$rang+1;
-            ?>
-        </td>
-        <td id="tdChaqueEquipe">
-            <?php
-                //Equipe
-                echo $nomEquipe = $equipe->nom;
-            ?>
-        </td>
-        <td>
-            <?php
-                //Points
-                //il faut que les buts de chaque match soient déjà calculés
-            ?>
-        </td>
-        <td>
-            <?php
-                //Joué
-                echo $equipe->games->count();
-            ?>
-        </td>
-        <td>
-            <?php
-            //Gagnés
-            foreach ($equipe->games as $game){
-                $butsPourDeChaqueMatch = $game->pivot->buts;
-                $game_id = $game->pivot->game_id;
-                $equipe_id = $game->pivot->equipe_id;
-                $autre_equipe_game = DB::table('equipe_game')->where([
-                    ['game_id','=', $game_id],
-                    ['equipe_id','!=', $equipe_id],
-                ])->get();
-                foreach ($autre_equipe_game as $autre_equipe_game){
-                    $butsContreDeChaqueMatch = $autre_equipe_game->buts;
-                }
-                $diffParMatch = $butsPourDeChaqueMatch - $butsContreDeChaqueMatch;
-                $gagnes = 0;
-                if ($diffParMatch>0){
-                    $gagneChaqueMatch = 1;
-                } else{
-                    $gagneChaqueMatch = 0;
-                }
-                echo $gagnes += $gagneChaqueMatch;
-            }
-
-            ?>
-        </td>
-        <td>
-            <?php
-            //Nuls
-            foreach ($equipe->games as $game){
-                $butsPourDeChaqueMatch = $game->pivot->buts;
-                $game_id = $game->pivot->game_id;
-                $equipe_id = $game->pivot->equipe_id;
-                $autre_equipe_game = DB::table('equipe_game')->where([
-                    ['game_id','=', $game_id],
-                    ['equipe_id','!=', $equipe_id],
-                ])->get();
-                foreach ($autre_equipe_game as $autre_equipe_game){
-                    $butsContreDeChaqueMatch = $autre_equipe_game->buts;
-                }
-                $diffParMatch = $butsPourDeChaqueMatch - $butsContreDeChaqueMatch;
-                $nuls = 0;
-                if ($diffParMatch=0){
-                    $nulChaqueMatch = 1;
-                } else{
-                    $nulChaqueMatch = 0;
-                }
-                echo $nuls += $nulChaqueMatch;
-            }
-
-            ?>
-        </td>
-        <td>
-            <?php
-            //Perdus
-            foreach ($equipe->games as $game){
-                $butsPourDeChaqueMatch = $game->pivot->buts;
-                $game_id = $game->pivot->game_id;
-                $equipe_id = $game->pivot->equipe_id;
-                $autre_equipe_game = DB::table('equipe_game')->where([
-                    ['game_id','=', $game_id],
-                    ['equipe_id','!=', $equipe_id],
-                ])->get();
-                foreach ($autre_equipe_game as $autre_equipe_game){
-                    $butsContreDeChaqueMatch = $autre_equipe_game->buts;
-                }
-                $diffParMatch = $butsPourDeChaqueMatch - $butsContreDeChaqueMatch;
-                $perdus = 0;
-                if ($diffParMatch<0){
-                    $perduChaqueMatch = 1;
-                } else{
-                    $perduChaqueMatch = 0;
-                }
-                echo $perdus += $perduChaqueMatch;
-            }
-
-            ?>
-        </td>
-        <td>
-            <?php
-                //buts pour
-                $butsPour = 0;
-                foreach ($equipe->games as $game){
-                    $butsPourDeChaqueMatch = $game->pivot->buts;
-                    $butsPour += $butsPourDeChaqueMatch;
-                }
-                echo $butsPour;
-            ?>
-        </td>
-        <td>
-            <?php
-                //buts contre
-                foreach ($equipe->games as $game){
-                    $game_id = $game->pivot->game_id;
-                    $equipe_id = $game->pivot->equipe_id;
-                    $autre_equipe_game = DB::table('equipe_game')->where([
-                        ['game_id','=', $game_id],
-                        ['equipe_id','!=', $equipe_id],
-                    ])->get();
-
-                    $butsContre = 0;
-                    foreach ($autre_equipe_game as $autre_equipe_game){
-                        $butsContreDeChaqueMatch = $autre_equipe_game->buts;
-                        $butsContre += $butsContreDeChaqueMatch;
-                    }
-                    echo $butsContre;
-                }
-            ?>
-        </td>
-        <td>
-            <?php
-                //diff.
-                echo $diff = $butsPour - $butsContre;
-        }
-            ?>
-        </td>
-
-
-    </tr>
+    @foreach($statsClassement as $statClassement)
+        <tr>
+            <td id="tdChaqueEquipe">{{ $statClassement['rang'] }}</td>
+            <td id="tdChaqueEquipe">{{ $statClassement['nomEquipe'] }}</td>
+            <td id="tdChaqueEquipe">{{ $statClassement['points'] }}</td>
+            <td id="tdChaqueEquipe">{{ $statClassement['joues'] }}</td>
+            <td id="tdChaqueEquipe">{{ $statClassement['gagnes'] }}</td>
+            <td id="tdChaqueEquipe">{{ $statClassement['nuls'] }}</td>
+            <td id="tdChaqueEquipe">{{ $statClassement['perdus'] }}</td>
+            <td id="tdChaqueEquipe">{{ $statClassement['butsPour'] }}</td>
+            <td id="tdChaqueEquipe">{{ $statClassement['butsContre'] }}</td>
+            <td id="tdChaqueEquipe">{{ $statClassement['diff'] }}</td>
+        </tr>
+    @endforeach
 </table>
