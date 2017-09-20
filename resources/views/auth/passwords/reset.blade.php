324b58d13208dd83edf5440ece7e3dea591153bc
LAPTOP-HBFJM11T\Ben <bj.delorme@gmail.com>
2017-09-21 01:43:46 +0200

    update reset password form view

--- resources/views/auth/passwords/reset.blade.php
+++ resources/views/auth/passwords/reset.blade.php
@@ -5,11 +5,11 @@
 @endsection
 
 @section('content')
-<div class="container-fluid content-login">
+<div class="container-fluid reset-password">
     <div class="row align-items-center">
-        <div class="col-md-6 offset-md-4">
+        <div class="col-md-6 offset-md-3">
             <div class="card">
-                <div class="card-header text-center">
+                <div class="card-header">
                     Réinitialisation du mot de passe
                 </div>
                 <div class="card-block">
@@ -27,7 +27,7 @@
                         <div class="form-group row{{ $errors->has('email') ? ' has-error' : '' }}">
                             <label for="email" class="sr-only control-label">Email</label>
                             <div class="offset-sm-2 col-sm-8">
-                                <input id="email" type="email" name="email" value="{{ $email or old('email') }}" class="form-control" placeholder="email" required>
+                                <input id="email" type="email" name="email" value="{{ $email or old('email') }}" class="form-control" placeholder="Adresse email" required>
                                 @if ($errors->has('email'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
@@ -39,7 +39,7 @@
                         <div class="form-group row{{ $errors->has('password') ? ' has-error' : '' }}">
                             <label for="inputPasswordForm" class="sr-only control-label">Mot de passe</label>
                             <div class="offset-sm-2 col-sm-8">
-                                <input type="password" name="password" class="form-control" id="inputPasswordForm" placeholder="mot de passe" required>
+                                <input type="password" name="password" class="form-control" id="inputPasswordForm" placeholder="Mot de passe" required>
                                 @if ($errors->has('password'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('password') }}</strong>
@@ -51,7 +51,7 @@
                         <div class="form-group row{{ $errors->has('password_confirmation') ? ' has-error' : '' }}">
                             <label for="password-confirm" class="sr-only control-label">{{ trans('passwords.password_confirm') }}</label>
                             <div class="offset-sm-2 col-sm-8">
-                                <input type="password" name="password" class="form-control" id="password-confirm" placeholder="mot de passe" required>
+                                <input type="password" name="password" class="form-control" id="password-confirm" placeholder="Confirmer mot de passe" required>
                                 @if ($errors->has('password_confirmation'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('password_confirmation') }}</strong>
@@ -62,8 +62,8 @@
 
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-green">
-                                    Reset Password
+                                <button type="submit" class="btn btn-orange btn-block">
+                                    Réinitialiser le mot de passe
                                 </button>
                             </div>
                         </div>
