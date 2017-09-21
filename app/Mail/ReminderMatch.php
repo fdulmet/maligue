71166b1fe0bfdc80b1f8765ab43aa422756b6fde
Benjamin DELORME <bd@tradelab.fr>
2017-09-21 15:00:37 +0200

    use markdown instead of view for build mail

--- app/Mail/ReminderMatch.php
+++ app/Mail/ReminderMatch.php
@@ -28,6 +28,6 @@ class ReminderMatch extends Mailable
      */
     public function build()
     {
-        return $this->view('view.name');
+        return $this->markdown('emails.reminders.match');
     }
 }
