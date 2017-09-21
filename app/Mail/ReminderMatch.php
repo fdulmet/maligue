1838b804d13ea40c07037bceb4fe79dee1d2986e
Benjamin DELORME <bd@tradelab.fr>
2017-09-21 16:55:23 +0200

    update reminders emails

--- app/Mail/ReminderMatch.php
+++ app/Mail/ReminderMatch.php
@@ -28,6 +28,6 @@ class ReminderMatch extends Mailable
      */
     public function build()
     {
-        return $this->view('emails.reminders.match');
+        return $this->markdown('emails.reminders.match');
     }
 }
--- resources/views/emails/reminders/goal.blade.php
+++ resources/views/emails/reminders/goal.blade.php
@@ -6,4 +6,5 @@ Vous n'avez pas encore rentré le score du match E1 - E2 du date heure.
 @component('mail::button', ['url' => config('app.url')])
     Entrer le score
 @endcomponent
+
 @endcomponent
