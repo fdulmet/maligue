c9561f4c82c29fdf6f3933f9b7edc50251e6853d
Benjamin DELORME <bd@tradelab.fr>
2017-09-21 15:26:33 +0200

    update reminders emails

--- resources/views/emails/reminders/goal.blade.php
+++ resources/views/emails/reminders/goal.blade.php
@@ -1,36 +1,9 @@
-@component('mail::layout')
-    {{-- Header --}}
-    @slot('header')
-        @component('mail::header', ['url' => config('app.url')])
-            <img src="{{ asset('images/logo_maligue.svg') }}">
-        @endcomponent
-    @endslot
+@component('mail::message')
+    Bonjour #NAME,
 
-    <p>
-        Bonjour #NAME,
-    </p>
-
-    <p>
-        Vous n'avez pas encore rentré le score du match #E1 - #E2 du #date #heure
-    </p>
+    Vous n'avez pas encore rentré le score du match #E1 - #E2 du #date #heure
 
     @component('mail::button', ['url' => config('app.url')])
         Entrer le score
     @endcomponent
-
-    {{-- Subcopy --}}
-    @if (isset($subcopy))
-        @slot('subcopy')
-            @component('mail::subcopy')
-                {{ $subcopy }}
-            @endcomponent
-        @endslot
-    @endif
-
-    {{-- Footer --}}
-    @slot('footer')
-        @component('mail::footer')
-            &copy; {{ date('Y') }} {{ config('app.name') }}. Tous droits réservés.
-        @endcomponent
-    @endslot
 @endcomponent
