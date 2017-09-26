472e7afc583bc4e6cb80dff563600b032605221c
LAPTOP-HBFJM11T\Ben <bj.delorme@gmail.com>
2017-09-26 09:46:43 +0200

    debug classement

--- app/Http/Helpers/Classement/Classer.php
+++ app/Http/Helpers/Classement/Classer.php
@@ -10,12 +10,19 @@ class Classer
     {
         $currentLigue = session('currentLigue');
 
+        $user = Auth::user();
+
+        if ($user->email == 'bj.delorme@gmail.com')
+        {
+
+        }
+
         $equipes = $currentLigue->equipes()->get();
         $rangParPoints = [];
 
         foreach ($equipes as $equipe)
         {
-            $data                   = new Data($equipe);
+            $data = new Data($equipe);
 
             //Rang
             if (!isset($rangParPoints[$data->points])) {
@@ -30,29 +37,36 @@ class Classer
             $rangParPoints[$data->points][$data->diff][$data->butsPour][] = $equipe;
         }
 
-        ksort($rangParPoints);// Sort by points
+        ksort($rangParPoints); // Sort by points
 
-        foreach ($rangParPoints as $i => $diff) {
+        foreach ($rangParPoints as $i => $diff)
+        {
             $diff = array_reverse($diff, true);
             $rangParPoints[$i] = $diff;
-            foreach ($diff as $butsPour) {
+            foreach ($diff as $butsPour)
+            {
                 ksort($butsPour);
             }
         }
 
-        function flatten(array $array)
-        {
-            $return = array();
-            array_walk_recursive($array, function ($a) use (&$return) {
-                $return[] = $a;
-            });
-            return $return;
-        }
-
-        $rangs = flatten($rangParPoints);
+        $rangs = self::flatten($rangParPoints);
 
         $rangs = array_reverse($rangs);
 
+        if ($user->email == 'bj.delorme@gmail.com')
+        {
+            dd($rangs);
+        }
+
         return $rangs;
+    }
+
+    static public function flatten(array $array)
+    {
+        $return = array();
+        array_walk_recursive($array, function ($a) use (&$return) {
+            $return[] = $a;
+        });
+        return $return;
     }
 }
