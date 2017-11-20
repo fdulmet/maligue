af72e8c9e44c36093ebfa3a59f8281399ae3813d
Nicolas Mérigot <nicolas.merigot@gmail.com>
2017-11-20 12:56:27 +0100

    Add password edit

--- resources/views/components/modals/user.blade.php
+++ resources/views/components/modals/user.blade.php
@@ -10,10 +10,10 @@ profilJoueur
   <form class="form-horizontal" role="form" method="POST" action="{{ route('user.update', ['userId' => Auth::user()->id])}}">
       {{ csrf_field() }}
       <div class="row form-group{{ $errors->has('last_name') ? ' has-danger' : '' }}">
-          <label for="last_name" class="col-md-4 control-label">Votre nom *</label>
+          <label for="last_name" class="col-md-4 control-label">Votre nom</label>
 
           <div class="col-md-6">
-              <input type="text" value="{{ Auth::user()->last_name }}" class="form-control" name="last_name" placeholder="" required>
+              <input type="text" value="{{ Auth::user()->last_name }}" class="form-control" name="last_name" placeholder="">
               @if($errors->has('last_name'))
                   <div class="form-control-feedback">{{ $errors->first('last_name') }}</div>
               @endif
@@ -21,10 +21,10 @@ profilJoueur
       </div>
 
       <div class="row form-group{{ $errors->has('first_name') ? ' has-danger' : '' }}">
-          <label for="first_name" class="col-md-4 control-label">Votre prénom *</label>
+          <label for="first_name" class="col-md-4 control-label">Votre prénom</label>
 
           <div class="col-md-6">
-              <input type="text" value="{{ Auth::user()->first_name }}" class="form-control" name="first_name" placeholder="" required>
+              <input type="text" value="{{ Auth::user()->first_name }}" class="form-control" name="first_name" placeholder="">
               @if($errors->has('first_name'))
                   <div class="form-control-feedback">{{ $errors->first('first_name') }}</div>
               @endif
@@ -32,10 +32,10 @@ profilJoueur
       </div>
 
       <div class="row form-group{{ $errors->has('email') ? ' has-danger' : '' }}">
-          <label for="email" class="col-md-4 control-label">Votre email *</label>
+          <label for="email" class="col-md-4 control-label">Votre email</label>
 
           <div class="col-md-6">
-              <input type="email" value="{{ Auth::user()->email }}" class="form-control" name="email" placeholder="" required>
+              <input type="email" value="{{ Auth::user()->email }}" class="form-control" name="email" placeholder="">
               @if($errors->has('email'))
                   <div class="form-control-feedback">{{ $errors->first('email') }}</div>
               @endif
@@ -43,13 +43,30 @@ profilJoueur
       </div>
 
       <div class="row form-group{{ $errors->has('phone') ? ' has-danger' : '' }}">
-          <label for="phone" class="col-md-4 control-label">Votre numéro de téléphone *</label>
+          <label for="phone" class="col-md-4 control-label">Votre numéro de téléphone</label>
 
           <div class="col-md-6">
-              <input type="phone" value="{{ Auth::user()->phone }}" class="form-control" name="phone" placeholder="" required>
+              <input type="text" value="{{ Auth::user()->phone }}" class="form-control" name="phone" placeholder="">
               @if($errors->has('phone'))
                   <div class="form-control-feedback">{{ $errors->first('phone') }}</div>
               @endif
+          </div>
+      </div>
+      <div class="row form-group{{ $errors->has('password') ? ' has-danger' : '' }}">
+          <label for="phone" class="col-md-4 control-label">Mot de passe</label>
+
+          <div class="col-md-6">
+              <input type="password" class="form-control" name="password" placeholder="">
+              @if($errors->has('password'))
+                  <div class="form-control-feedback">{{ $errors->first('password') }}</div>
+              @endif
+          </div>
+      </div>
+      <div class="row form-group">
+          <label for="password_confirmation" class="col-md-4 control-label">Confirmer votre mot de passe</label>
+
+          <div class="col-md-6">
+              <input type="password" class="form-control" name="password_confirmation" placeholder="">
           </div>
       </div>
       <button type="submit" class="btn btn-orange btn-block">Mettre à jour</button>
