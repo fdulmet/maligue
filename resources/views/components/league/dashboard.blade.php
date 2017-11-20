9afe20c6a31cac0de6675046a645f4dbc8bddd32
Nicolas Mérigot <nicolas.merigot@gmail.com>
2017-11-20 12:52:40 +0100

    Add hack for old seasons

--- resources/views/components/league/dashboard.blade.php
+++ resources/views/components/league/dashboard.blade.php
@@ -3,6 +3,15 @@
 @include('components.league.modals.invite_to_league')
 @include('components.league.modals.reward')
 @include('components.modals.delay_rules')
+@if ($league->slug === 'la-ligue-des-pinguoins')
+  @include('components.season.modals.oldseasons.2011-2010')
+  @include('components.season.modals.oldseasons.2012-2011')
+  @include('components.season.modals.oldseasons.2013-2012')
+  @include('components.season.modals.oldseasons.2014-2013')
+  @include('components.season.modals.oldseasons.2015-2014')
+  @include('components.season.modals.oldseasons.2016-2015')
+  @include('components.season.modals.oldseasons.2017-2016')
+@endif
 <div class="row header-saison">
     <div class="col-md-12">
         <h1>
@@ -22,15 +31,15 @@
                                                                                                   'seasonSlug' => $s->slug,
                                                                                                   'teamSlug' => $team->slug]) }}">{{ $s->name }}</a>
                     @endforeach
-                    <!--
-                    <a class="dropdown-item" href="http://laliguedespingouins.fr/" target="_blank">2016-17</a>
-                    <a class="dropdown-item" href="http://laliguedespingouins.fr/2015-2016.php" target="_blank">2015-16</a>
-                    <a class="dropdown-item" href="http://laliguedespingouins.fr/2014-2015.php" target="_blank">2014-15</a>
-                    <a class="dropdown-item" href="http://laliguedespingouins.fr/2013-2014.php" target="_blank">2013-14</a>
-                    <a class="dropdown-item" href="http://laliguedespingouins.fr/2012-2013.php" target="_blank">2012-13</a>
-                    <a class="dropdown-item" href="http://laliguedespingouins.fr/2011-2012.php" target="_blank">2011-12</a>
-                    <a class="dropdown-item" href="http://laliguedespingouins.fr/2010-2011.php" target="_blank">2010-11</a>
-                  -->
+                    @if ($league->slug === 'la-ligue-des-pinguoins')
+                      <a class="dropdown-item" data-toggle="modal" data-target="#oldSeason-2017-2016">2016-2017</a>
+                      <a class="dropdown-item" data-toggle="modal" data-target="#oldSeason-2016-2015">2015-2016</a>
+                      <a class="dropdown-item" data-toggle="modal" data-target="#oldSeason-2015-2014">2014-2015</a>
+                      <a class="dropdown-item" data-toggle="modal" data-target="#oldSeason-2014-2013">2013-2014</a>
+                      <a class="dropdown-item" data-toggle="modal" data-target="#oldSeason-2013-2012">2012-2013</a>
+                      <a class="dropdown-item" data-toggle="modal" data-target="#oldSeason-2012-2011">2011-2012</a>
+                      <a class="dropdown-item" data-toggle="modal" data-target="#oldSeason-2011-2010">2010-2011</a>
+                    @endif
                 </div>
             </div>
         </h1>
---
+++ resources/views/components/season/modals/oldseasons/2014-2013.blade.php
@@ -0,0 +1,12 @@
+@component('components.modals.large')
+@slot('id')
+oldSeason-2014-2013
+@endslot
+@slot('title')
+    2013-2014
+@endslot
+<div class="container">
+    <iframe src="{{url('/old_seasons/2014-2013.html')}}" style='width:100%;min-height: 25em;'>
+    </iframe>
+</div>
+@endcomponent
