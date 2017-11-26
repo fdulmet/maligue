8e406bb617d290cb13343bf444650eee848e254d
Nicolas Mérigot <nicolas.merigot@gmail.com>
2017-11-26 16:29:37 +0100

    Fix: improve logo modal appereance

--- resources/views/components/team/modals/update_logo.blade.php
+++ resources/views/components/team/modals/update_logo.blade.php
@@ -1,3 +1,5 @@
+<form class="form-horizontal" enctype="multipart/form-data" role="form" method="POST" action="{{ route('team.update', ['teamSlug' => $team->slug]) }}" id="formUpdateTeamLogo">
+    {{ csrf_field() }}
 @component('components.modals.base')
 @slot('id')
 updateTeamLogo
@@ -7,13 +9,12 @@ updateTeamLogo
 @endslot
 <div class="row">
     <div class="col-md-8 col-md-offset-2">
-      <form class="form-horizontal" enctype="multipart/form-data" role="form" method="POST" action="{{ route('team.update', ['teamSlug' => $team->slug]) }}" id="formUpdateTeamLogo">
-          {{ csrf_field() }}
+
 
             <div class="form-group">
               <div class="fileupload fileupload-new" data-provides="fileupload">
                   <span class="btn btn-orange btn-file">
-                  <span class="fileupload-new">Choisir image</span>
+                  <span class="fileupload-new">Choisir l'image</span>
                   <span class="fileupload-exists">Choisir une autre image</span>
                       <input name="logo" type="file">
                   </span>
@@ -21,13 +22,15 @@ updateTeamLogo
                   <a href="#" class="close fileupload-exists" data-dismiss="fileupload" style="float: none">×</a>
               </div>
             </div>
-            <div class="form-group">
-                <button type="submit" class="btn btn-orange btn-block" id="bouton_submit">
-                    Mettre à jour le logo
-                </button>
-            </div>
+
         </form>
     </div>
 </div>
-
+@slot('footer')
+<div class="form-group">
+    <button type="submit" class="btn btn-orange btn-block" id="bouton_submit">
+        Modifier
+    </button>
+</div>
+@endslot
 @endcomponent
