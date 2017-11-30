89f97c9433baeffc23520559e27341adc8755cc3
Nicolas Mérigot <nicolas.merigot@gmail.com>
2017-11-30 18:00:02 +0100

    Remove title

--- resources/views/components/league/modals/reward.blade.php
+++ resources/views/components/league/modals/reward.blade.php
@@ -7,7 +7,6 @@ reward
 @endslot
 <div class="row">
     <div class="col-md-12">
-        Cette saison, vous pouvez gagner :
         <ul>
           <li>Un pichet d’1,5L de bière est offert à l’équipe victorieuse de chaque match (à partager en cas de match nul !).</li>
           <li>Toutes les équipes peuvent bénéficier de 10% de remise sur les équipements Adidas à partir d'une commande pour 5 joueurs</li>
